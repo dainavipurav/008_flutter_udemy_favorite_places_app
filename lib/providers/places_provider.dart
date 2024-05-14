3b27816ceb6d126fce18bda10f6671d260703df3
dainavi.purav <dainavi.purav@cmss.in>
2024-05-15 02:11:39 +0530

    provider code modified and enhanced

--- lib/providers/places_provider.dart
+++ lib/providers/places_provider.dart
@@ -1,10 +1,17 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:udemy_007_favorite_places_app/models/place.dart';
 
-final places = [];
-
 class PlaceNotifier extends StateNotifier<List<Place>> {
   PlaceNotifier() : super([]);
+
+  void addPlace(String title) {
+    final newPlace = Place(title: title);
+
+    state = [
+      newPlace,
+      ...state,
+    ];
+  }
 }
 
 final placesProvider = StateNotifierProvider<PlaceNotifier, List<Place>>((ref) {
