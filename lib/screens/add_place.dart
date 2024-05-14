db227c9ae54dd6c9ec6fdbdd3a4634a6ec85ec73
dainavi.purav <dainavi.purav@cmss.in>
2024-05-15 02:11:22 +0530

    code modified as per tutorial video

--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -1,22 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:udemy_007_favorite_places_app/models/place.dart';
-import 'package:udemy_007_favorite_places_app/providers/places_provider.dart';
 
-class AddPlaceScreen extends ConsumerWidget {
+class AddPlaceScreen extends StatefulWidget {
   const AddPlaceScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final textController = TextEditingController();
-    final wref = ref.watch(placesProvider);
-    final formKey = GlobalKey<FormState>();
+  State<AddPlaceScreen> createState() {
+    return _AddPlaceScreenState();
+  }
+}
 
+class _AddPlaceScreenState extends State<AddPlaceScreen> {
+  final _textController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+  @override
+  void dispose() {
+    _textController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Add new Place'),
       ),
-      body: Padding(
+      body: SingleChildScrollView(
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
         child: Column(
           mainAxisSize: MainAxisSize.min,
@@ -24,43 +32,26 @@ class AddPlaceScreen extends ConsumerWidget {
             Form(
               key: formKey,
               child: TextFormField(
-                decoration: InputDecoration(
-                  label: Text(
-                    'Title',
-                    style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                          color: Theme.of(context).colorScheme.onBackground,
-                        ),
-                  ),
-                  labelStyle: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                        color: Theme.of(context).colorScheme.onBackground,
-                      ),
+                decoration: const InputDecoration(label: Text('Title')),
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.onBackground,
                 ),
-                style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                      color: Theme.of(context).colorScheme.onBackground,
-                    ),
                 validator: (value) {
                   if (value == null || value.isEmpty) {
                     return 'Please enter place name';
                   }
                   return null;
                 },
-                onSaved: (value) {},
                 keyboardType: TextInputType.text,
-                controller: textController,
+                controller: _textController,
               ),
             ),
             const SizedBox(height: 20),
-            TextButton.icon(
+            ElevatedButton.icon(
               icon: const Icon(Icons.add),
               onPressed: () {
                 if (formKey.currentState!.validate()) {
                   formKey.currentState!.save();
-                  Place model = Place(
-                    title: textController.text,
-                  );
-                  wref.add(model);
-
-                  print('${model.id} : ${model.title}');
 
                   Navigator.pop(context);
                 }
