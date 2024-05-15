44b94a20fcbfa92d937fc9f79e2d739c1cf4f822
dainavi.purav <dainavi.purav@cmss.in>
2024-05-16 04:28:08 +0530

    refactor : UI Picked Location preview image added

--- lib/screens/place_detail.dart
+++ lib/screens/place_detail.dart
@@ -5,6 +5,12 @@ class PlaceDetailScreen extends StatelessWidget {
   final Place place;
   const PlaceDetailScreen({super.key, required this.place});
 
+  String get locationImage {
+    final lat = place.location.latitude;
+    final lng = place.location.longitude;
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$lng&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$lat,$lng&key=AIzaSyDE4KNcnxF9J-BC8wRvZjFDYrSwxt8epLo';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,6 +25,40 @@ class PlaceDetailScreen extends StatelessWidget {
             height: double.infinity,
             width: double.infinity,
           ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: Column(
+              children: [
+                CircleAvatar(
+                  radius: 70,
+                  backgroundImage: NetworkImage(locationImage),
+                ),
+                Container(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+                  alignment: Alignment.center,
+                  decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                    colors: [
+                      Colors.transparent,
+                      Colors.black54,
+                    ],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                  )),
+                  child: Text(
+                    place.location.address,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                          color: Theme.of(context).colorScheme.onBackground,
+                        ),
+                  ),
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
