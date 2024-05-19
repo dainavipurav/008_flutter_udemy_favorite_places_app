87ed7b6d9a26ad51410de29f4d360db7b5b4ae4f
dainavi.purav <dainavi.purav@cmss.in>
2024-05-19 15:52:18 +0530

    refactor : updated model with id , functionality added to storing places in device storage

--- lib/providers/user_places_provider.dart
+++ lib/providers/user_places_provider.dart
@@ -3,12 +3,52 @@ import 'dart:io';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:path_provider/path_provider.dart' as syspaths;
 import 'package:path/path.dart' as path;
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:sqflite/sqlite_api.dart';
+
 import 'package:udemy_007_favorite_places_app/models/place.dart';
+
+Future<Database> _getDataBase() async {
+  final dbPath = await sql.getDatabasesPath();
+  final db = await sql.openDatabase(
+    path.join(dbPath, 'places.db'),
+    version: 1,
+    onCreate: (database, version) {
+      return database.execute(
+        'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT,image TEXT, lat REAL, lng REAL, address TEXT)',
+      );
+    },
+  );
+
+  return db;
+}
 
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
   UserPlacesNotifier() : super([]);
 
-  void addPlace(String title, File image, PlaceLocation location) async {
+  void loadPlaces() async {
+    final db = await _getDataBase();
+    final data = await db.query('user_places');
+
+    final places = data
+        .map(
+          (row) => Place(
+            title: row['title'] as String,
+            image: File(row['image'] as String),
+            location: PlaceLocation(
+              address: row['address'] as String,
+              latitude: row['lat'] as double,
+              longitude: row['lng'] as double,
+            ),
+          ),
+        )
+        .toList();
+
+    state = places;
+  }
+
+  Future<void> addPlace(
+      String title, File image, PlaceLocation location) async {
     final appDir = await syspaths.getApplicationDocumentsDirectory();
     final fileName = path.basename(image.path);
     final copiedImage = await image.copy('${appDir.path}/$fileName');
@@ -17,6 +57,20 @@ class UserPlacesNotifier extends StateNotifier<List<Place>> {
       title: title,
       image: copiedImage,
       location: location,
+    );
+
+    final db = await _getDataBase();
+
+    db.insert(
+      'user_places',
+      {
+        'id': newPlace.id,
+        'title': newPlace.title,
+        'image': newPlace.image.path,
+        'lat': newPlace.location.latitude,
+        'lng': newPlace.location.longitude,
+        'address': newPlace.location.address,
+      },
     );
 
     state = [
